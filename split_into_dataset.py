0523fff382287216b4b77d7084cd298a64b7e203
Adrial-Knight <pierreminier64@gmail.com>
2023-02-23 10:53:53 +0100

    [Split] [Clean]
    1. ajout de la fonction permettant de copier les fichiers vers une nouvelle base compte tenu de la partition réalisée
    2. fonction display --> savefig car les figures sont enregistrées sur le disque
    3. 3 fonctions explicites pour les méthodes PSP, IFP et IFPO (bien nommées, comme dans le rapport)
    4. les fonctions d'optimisation renvoient un dictionnaire de métrique plutot que toutes les métriques à la volée --> code plus propre

--- split_into_dataset.py
+++ split_into_dataset.py
@@ -3,49 +3,70 @@ from PIL import Image
 import matplotlib.pyplot as plt
 import numpy as np
 import json
+import shutil
 
+
+FIG_FOLDER = "figures"
 
 def get_families(path):
     fnames = [fname.split(".")[0] for fname in os.listdir(path)]
     patchs = [fname.rsplit("_", 1) for fname in fnames]
     label  = path.rsplit("/", 1)[-1]
-    family_info = {"path": path, "label": label, "files": {}}
+    family_infos = {"path": path, "label": label, "files": {}}
     for element in patchs:
         key = element[0]
         value = int(element[1])
-        if key in family_info["files"]:
-            family_info["files"][key].append(value)
+        if key in family_infos["files"]:
+            family_infos["files"][key].append(value)
         else:
-            family_info["files"][key] = [value]
-    return family_info
+            family_infos["files"][key] = [value]
+    return family_infos
 
-def compute_proportions(family_info):
-    path = family_info["path"]
-    for fam_name, patches in family_info["files"].items():
-        family_info[fam_name] = 0
+def compute_proportions(family_infos):
+    path = family_infos["path"]
+    for fam_name, patches in family_infos["files"].items():
+        family_infos[fam_name] = 0
         for patch_id in patches:
             with Image.open(f"{path}/{fam_name}_{patch_id}.jpg") as img:
                 W, H = img.size
-                family_info[fam_name] += W * H
+                family_infos[fam_name] += W * H
 
-    categories = list(family_info["files"].keys())
-    total = sum([family_info[key] for key in categories])
-    proportions = [family_info[key]/total for key in categories]
+    categories = list(family_infos["files"].keys())
+    total = sum([family_infos[key] for key in categories])
+    proportions = [family_infos[key]/total for key in categories]
 
     d = {"category":categories, "proportions":proportions}
     sorted_data = sorted(zip(d["category"], d["proportions"]), key=lambda x: x[1], reverse=True)
     sorted_labels, sorted_proportions = zip(*sorted_data)
 
-    family_info["category"] = list(sorted_labels)
-    family_info["proportions"] = list(sorted_proportions)
+    family_infos["category"] = list(sorted_labels)
+    family_infos["proportions"] = list(sorted_proportions)
 
+def split_PSP(family_infos, p=[.7, .15, .15]):
+    categories  = family_infos["category"].copy()
+    proportions = family_infos["proportions"].copy()
+    parts = [{"category": [], "capacity": c} for c in p]
 
-def split_categories(family_info, p=[.7, .15, .15], mu=1, extra_capacity=0):
-    categories = family_info["category"].copy()
-    proportions = family_info["proportions"].copy()
+    def affect(proportions, categories, P):
+        original_capacity = P["capacity"]
+        while P["capacity"] > 0 and categories:
+            P["category"].append(categories.pop(0))
+            P["capacity"] -= proportions.pop(0)
+        P["capacity"] = original_capacity - P["capacity"]
+
+    for P in parts:
+        affect(proportions, categories, P)
+
+    return parts
+
+def split_IFP(family_infos, p=[.7, .15, .15], mu=1):
+    return split_IFPO(family_infos, p, mu, extra_capacity=0)
+
+def split_IFPO(family_infos, p=[.7, .15, .15], mu=1, extra_capacity=0):
+    categories = family_infos["category"].copy()
+    proportions = family_infos["proportions"].copy()
     parts = [{"category": [], "capacity": c} for c in p]
     total_categories = len(categories)
-    for P in parts[1:]: P["capacity"] += extra_capacity
     threshold = mu*min(parts[1]["capacity"], parts[2]["capacity"])
     while categories and proportions[0] > threshold:
         parts[0]["category"].append(categories.pop(0))
@@ -69,6 +90,7 @@ def split_categories(family_info, p=[.7, .15, .15], mu=1, extra_capacity=0):
                 else:
                     deficits[i] = 0
 
+    for P in parts[1:]: P["capacity"] += extra_capacity
     pending = [True, True, True]
     while categories and any(pending):
         for i, P in enumerate(parts):
@@ -86,74 +108,110 @@ def split_categories(family_info, p=[.7, .15, .15], mu=1, extra_capacity=0):
 
     return parts, capacity, diversity
 
-def display_balance_partition(family_infos, parts):
-    fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(9, 4))
-
-    axes[0].pie(family_infos["proportions"], labels=family_infos["category"], autopct="%1.1f%%")
-    axes[0].set_title(f"{family_infos['label']} Class Balance")
-
-    for i, prop in enumerate(family_infos["proportions"]):
-        if prop < 0.05:
-            plt.setp(axes[0].texts[2*i], visible=False)
-            plt.setp(axes[0].texts[2*i+1], visible=False)
-
-    proportions = [P["capacity"] if P["capacity"] > 0 else 0 for P in parts]
-    datasets = ["Training", "Validation", "Test"]
-    labels = [f"{dataset} ({len(P['category'])})" for dataset, P in zip(datasets, parts)]
-    axes[1].pie(proportions, labels=labels, autopct="%1.1f%%")
-    axes[1].set_title(f"{family_infos['label']} Partitions")
-
-def display_loss(family_infos, loss_capacity, loss_diversity, mu_list):
-    fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(9, 4))
-    mean_loss = (loss_capacity + loss_diversity) / 2
-    ax.plot(mu_list, loss_capacity, label="Loss Capacity")
-    ax.plot(mu_list, loss_diversity, label="Loss Diversity")
-    ax.plot(mu_list, mean_loss, label="Loss Mean")
-    ax.set_xlabel("$\mu$")
-    ax.set_ylabel("Loss")
-    ax.set_title(f"{family_infos['label']} Class - Loss Plot")
-    ax.grid()
-    ax.legend()
-
-def search1D(family_infos, prop, mu_list, extra_capacity):
+def optimize_IFP(family_infos, prop, mu_list):
     loss_capacity  = np.zeros((len(mu_list)))
     loss_diversity = np.zeros((len(mu_list)))
     for i_mu, mu in enumerate(mu_list):
-        parts, capacity, diversity = split_categories(family_infos, prop, mu, extra_capacity)
+        parts, capacity, diversity = split_IFP(family_infos, prop, mu)
         loss_capacity[i_mu]  = np.mean(np.abs(capacity - prop))
         loss_diversity[i_mu] = np.mean(np.abs(diversity - 1/3))
     i_min = np.argmin(loss_capacity + loss_diversity)
-    parts, _, _ = split_categories(family_infos, prop, mu_list[i_min], extra_capacity)
-    return parts, loss_capacity, loss_diversity
+    mu_opt = mu_list[i_min]
+    parts, _, _ = split_IFP(family_infos, prop, mu_opt)
+    metrics = {"capacity": loss_capacity,
+               "diversity": loss_diversity,
+               "mu": mu_opt}
+    return parts, metrics
 
-def search2D(family_infos, prop, mu_list, extra_capacity_list):
+def optimize_IFPO(family_infos, prop, mu_list, extra_capacity_list):
     heatmap_capacity  = np.zeros((len(mu_list), len(extra_capacity_list)))
     heatmap_diversity = np.zeros((len(mu_list), len(extra_capacity_list)))
     for i_mu, mu in enumerate(mu_list):
         for i_xc, xc in enumerate(extra_capacity_list):
-            _, capacity, diversity = split_categories(family_infos, prop, mu, xc)
+            _, capacity, diversity = split_IFPO(family_infos, prop, mu, xc)
             heatmap_capacity[i_mu, i_xc]  = np.mean(np.abs(capacity - prop))
             heatmap_diversity[i_mu, i_xc] = np.mean(np.abs(diversity - 1/3))
     min_idx = np.argmin(heatmap_capacity + heatmap_diversity)
     min_idx_2d = np.unravel_index(min_idx, heatmap_capacity.shape)
-    parts, _, _ = split_categories(family_infos, prop, mu_list[min_idx_2d[0]], extra_capacity_list[min_idx_2d[1]])
-    return parts, heatmap_capacity, heatmap_diversity
+    mu_opt, eps_opt = mu_list[min_idx_2d[0]], extra_capacity_list[min_idx_2d[1]]
+    parts, _, _ = split_IFPO(family_infos, prop, mu_opt, eps_opt)
+    metrics = {"capacity": heatmap_capacity,
+               "diversity": heatmap_diversity,
+               "mu": mu_opt,
+               "eps": eps_opt}
+    return parts, metrics
 
-def display_heatmaps(heatmap_capacity, heatmap_diversity, mu_list, extra_capacity_list):
-    fig, axs = plt.subplots(nrows=1, ncols=2, figsize=(9, 4))
-    im = []
-    im.append(axs[0].imshow(heatmap_capacity, cmap="coolwarm", origin="lower",
-                       extent=[extra_capacity_list[0], extra_capacity_list[-1], mu_list[0], mu_list[-1]]))
-    im.append(axs[1].imshow(heatmap_diversity,cmap="coolwarm", origin="lower",
-                       extent=[extra_capacity_list[0], extra_capacity_list[-1], mu_list[0], mu_list[-1]]))
-    for i, lab in enumerate(["Capacity", "Diversity"]):
-        axs[i].set_xlabel("Extra Capacity")
-        axs[i].set_ylabel("$\mu$")
-        axs[i].set_title(f"Loss {lab}")
-        axs[i].set_aspect("auto")
-        fig.colorbar(im[i], ax=axs[i])
+def print_parts(parts, method="", mu=None, eps=None):
+    capacity  = tuple([int(P["capacity"]*10000)/100 for P in parts])
+    diversity = tuple([len(P["category"]) for P in parts])
+    print(f"\t{method}:")
+    print(f"\t{capacity= }")
+    print(f"\t{diversity=}")
+    if mu: mu=round(mu, 2)
+    if eps: eps=round(eps, 2)
+    print(f"\t{(mu, eps)=}\n")
 
-def split(db, prop=[.7, .15, .15], class_names=["PD", "D", "ED"]):
+def savefig_family_repartition(family_infos):
+    fig, ax = plt.subplots(num=f"{family_infos['label']} Class Balance", nrows=1, ncols=1)
+    labels  = [f"f{i}" for i in range(1, len(family_infos["category"])+1)]
+    ax.pie(family_infos["proportions"], labels=labels,
+           autopct="%1.1f%%", textprops={"fontsize": 9})
+    for i, prop in enumerate(family_infos["proportions"]):
+        if prop < 0.05:
+            plt.setp(ax.texts[2*i],   visible=False)
+            plt.setp(ax.texts[2*i+1], visible=False)
+    fig.subplots_adjust(top=1, bottom=0, right=1, left=0, hspace=0, wspace=0)
+    ax.margins(0, 0)
+    fig.set_size_inches(2.5, 2.5)
+    plt.savefig(f"{FIG_FOLDER}/class_balance_{family_infos['label']}.pdf",
+                format="pdf", facecolor=None, pad_inches=0, bbox_inches="tight",
+                dpi=1200)
+    return fig
+
+def savefig_partition(family_infos, parts, method):
+    fig, ax = plt.subplots(num=f"{method} Partitioning - {family_infos['label']} Class", nrows=1, ncols=1)
+    proportions = [P["capacity"] if P["capacity"] > 0 else 0 for P in parts]
+    datasets = ["P1", "P2", "P3"]
+    labels = [f"{dataset} ({len(P['category'])})" for dataset, P in zip(datasets, parts)]
+    ax.pie(proportions, labels=labels, autopct="%1.1f%%", textprops={"fontsize": 11})
+    ax.axis("equal")
+    fig.subplots_adjust(top=1, bottom=0, right=1, left=0, hspace=0, wspace=0)
+    ax.margins(0, 0)
+    fig.set_size_inches(2.5, 2.5)
+    plt.savefig(f"{FIG_FOLDER}/partition_{method}_{family_infos['label']}.pdf",
+                format="pdf", facecolor=None, pad_inches=0, bbox_inches="tight",
+                dpi=1200)
+    return fig
+
+def savefig_loss(family_infos, loss_metrics, mu_list):
+    fig, ax = plt.subplots(num=f"IFP Loss - {family_infos['label']} Class", nrows=1, ncols=1, figsize=(9, 4))
+    loss_capacity = loss_metrics["capacity"]
+    loss_diversity = loss_metrics["diversity"]
+    mean_loss = (loss_capacity + loss_diversity) / 2
+    ax.plot(mu_list, loss_capacity, label="Loss Capacity")
+    ax.plot(mu_list, loss_diversity, label="Loss Diversity")
+    ax.plot(mu_list, mean_loss, linestyle="--", label="Loss Mean")
+    ax.set_xlabel("$\mu$")
+    ax.set_ylabel("Loss")
+    ax.grid()
+    ax.legend()
+    plt.savefig(f"{FIG_FOLDER}/loss_IFP_{family_infos['label']}.pdf", format="pdf", bbox_inches="tight", pad_inches=0)
+    return fig
+
+def savefig_heatmap(family_infos, metrics, mu_list, eps_list, type):
+    heatmap = metrics[type]
+    fig, ax = plt.subplots(num=f"Heatmap {type} - {family_infos['label']} Class")
+    im = plt.imshow(heatmap, cmap="coolwarm", origin="lower",
+                    extent=[eps_list[0], eps_list[-1], mu_list[0], mu_list[-1]])
+    ax.set_xlabel("Extra Capacity $\epsilon$")
+    ax.set_ylabel("$\mu$")
+    ax.set_aspect("auto")
+    fig.colorbar(im)
+    plt.savefig(f"{FIG_FOLDER}/heatmap_{type}_{family_infos['label']}.pdf",
+                format="pdf", bbox_inches="tight", pad_inches=0)
+    return fig
+
+def split_files(db, prop=[.7, .15, .15], class_names=["PD", "D"]):
     class_infos = {}
     for cname in class_names:
         family_infos = get_families(path=f"{db}/{cname}")
@@ -161,59 +219,85 @@ def split(db, prop=[.7, .15, .15], class_names=["PD", "D", "ED"]):
         class_infos[cname] = family_infos
 
     mu_list = np.linspace(0, 1, 1000)
-    extra_capacity_list = np.linspace(0, 0.1, 100)
+    eps_list = np.linspace(0, 0.1, 100)
     partitions = {}
     for cname, family_infos in class_infos.items():
-        # parts, _, _ = search1D(family_infos, prop, mu_list, extra_capacity=.05)
-        parts, _, _ = search2D(family_infos, prop, mu_list, extra_capacity_list)
+        parts, _ = optimize_IFPO(family_infos, prop, mu_list, eps_list)
         partitions[cname] = parts
 
-    datasets = {"training":   {"files": {}, "pixels": {}},
-                "validation": {"files": {}, "pixels": {}},
-                "test":       {"files": {}, "pixels": {}}}
+    datasets = {"training":   {"files": {}, "nb_im": {}},
+                "validation": {"files": {}, "nb_im": {}},
+                "test":       {"files": {}, "nb_im": {}}}
     for dataset_id, dataset in enumerate(datasets.values()):
         for cname, parts in partitions.items():
             patterns = parts[dataset_id]["category"]
             path = f"{db}/{cname}"
             files = [file for file in os.listdir(path) if any(file.startswith(pattern) for pattern in patterns)]
             dataset["files"][cname] = files
-            dataset["pixels"][cname] = 0
-            for fname in files:
-                with Image.open(f"{path}/{fname}") as img:
-                    W, H = img.size
-                    dataset["pixels"][cname] += W * H
+            dataset["nb_im"][cname] = len(files)
 
     with open(f"{db}/split.json", "w") as fd:
         json.dump(datasets, fd, sort_keys=True, indent=4)
     return datasets
 
+def assign_files(old_root: str, datasets: json):
+    new_root = old_root + "_split"
+    os.makedirs(new_root, exist_ok=True)
+    for dataset_id, infos in datasets.items():
+        classes = infos["files"]
+        dataset_dir = f"{new_root}/{dataset_id}"
+        for class_id, files in classes.items():
+            old_class_dir = f"{old_root}/{class_id}"
+            new_class_dir = f"{dataset_dir}/{class_id}"
+            os.makedirs(new_class_dir, exist_ok=True)
+            for file in files:
+                src_file = os.path.join(old_class_dir, file)
+                dst_file = os.path.join(new_class_dir, file)
+                shutil.copy(src_file, dst_file)
+    with open(f"{new_root}/split.json", "w") as fd:
+        json.dump(datasets, fd, sort_keys=True, indent=4)
 
 def test(db):
     class_infos = {}
     prop = [.7, .15, .15]
-    for cname in ["PD", "D", "ED"]:
+    for cname in ["PD", "D"]:
         family_infos = get_families(path=f"{db}/{cname}")
         compute_proportions(family_infos)
         class_infos[cname] = family_infos
 
-    family_infos = class_infos["ED"]
+    os.makedirs(FIG_FOLDER, exist_ok=True)
 
-    # 1D: adjust mu
-    mu_list = np.linspace(0, 1, 1000)
-    extra_capacity = 0.05
-    parts, loss_capacity, loss_diversity = search1D(family_infos, prop, mu_list, extra_capacity)
-    display_balance_partition(family_infos, parts)
-    display_loss(family_infos, loss_capacity, loss_diversity, mu_list)
+    for cname in ["D", "PD"]:
+        family_infos = class_infos[cname]
+        total_families = len(family_infos["files"].keys())
+        print(f"{cname} Class: {total_families} families")
+        savefig_family_repartition(family_infos)
 
-    # 2D: adjust mu and extra_capacity
-    extra_capacity_list = np.linspace(0, 0.1, 100)
-    parts, heatmap_capacity, heatmap_diversity = search2D(family_infos, prop, mu_list, extra_capacity_list)
-    display_balance_partition(family_infos, parts)
-    display_heatmaps(heatmap_capacity, heatmap_diversity, mu_list, extra_capacity_list)
+        # PSP: Proportional and Sequential Partitioning
+        parts = split_PSP(family_infos, prop)
+        print_parts(parts, method="PSP")
+        savefig_partition(family_infos, parts, method="PSP")
 
-    plt.show()
+        # IFP: Iterative Filling Partitioning (1 HP)
+        mu_list = np.linspace(0, 1, 1000)
+        parts, metrics = optimize_IFP(family_infos, prop, mu_list)
+        print_parts(parts, method="IFP", mu=metrics["mu"])
+        savefig_partition(family_infos, parts, method="IFP")
+        savefig_loss(family_infos, metrics, mu_list)
+
+        # IFPO: Iterative Filling Partitioning with Overfill (2 HP)
+        eps_list = np.linspace(0, 0.1, 100)
+        parts, metrics = optimize_IFPO(family_infos, prop, mu_list, eps_list)
+        print_parts(parts, method="IFP0", mu=metrics["mu"], eps=metrics["eps"])
+        savefig_partition(family_infos, parts, method="IFPO")
+        savefig_heatmap(family_infos, metrics, mu_list, eps_list, type="capacity")
+        savefig_heatmap(family_infos, metrics, mu_list, eps_list, type="diversity")
+
+    # plt.show()
 
 
 if __name__ == "__main__":
-    # test(db="database/224x224")
-    split(db="database/224x224")
+    db="database/224x224"
+    # test(db)
+    datasets = split_files(db)
+    assign_files(db, datasets)
