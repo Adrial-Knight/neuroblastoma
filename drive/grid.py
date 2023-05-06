173f5985240e7dd51253d33f422f1ac647b45473
Adrial-Knight <pierreminier64@gmail.com>
2023-05-06 12:13:49 +0200

    [Drive] [Grille]
    Grille présentant les recherches effectuées et leurs avancements pour un model donné

---
+++ drive/grid.py
@@ -0,0 +1,66 @@
+import pydrive_wrap as Gdrive
+import numpy as np
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+
+SKIP_FOLDER = ["__Summary__"]
+
+def main(drive, model):
+    grid = make_grid(drive, model)
+    display_grid(grid, model)
+
+def make_grid(drive, model):
+    root_path = f"Stage_Bilbao_Neuroblastoma/G_Collab/backup/{model}"
+    root_id   = Gdrive.get_id_from_path(drive, root_path)
+    title_list, id_list = Gdrive.list_from_id(drive, root_id)
+    grid = {}
+    for title, id in tqdm(zip(title_list, id_list), total=len(id_list)):
+        if title in SKIP_FOLDER: continue
+        exp_list, _ = Gdrive.list_from_id(drive, id)
+        lr, batch = title.split("_")
+        value = len(exp_list)
+        if "tmp" in exp_list: value -= 1
+        grid[(lr, batch)] = value
+    return grid
+
+def display_grid(grid, model):
+    # Récupérer les coordonnées et les valeurs
+    coordinates = list(grid.keys())
+    values = list(grid.values())
+
+    # Convertir les coordonnées en nombres pour créer la carte
+    x_labels = list(set([float(coord[0]) for coord in coordinates]))
+    y_labels = list(set([int(coord[1]) for coord in coordinates]))
+    x_labels.sort()
+    y_labels.sort()
+    x_indices = [x_labels.index(float(coord[0])) for coord in coordinates]
+    y_indices = [y_labels.index(int(coord[1])) for coord in coordinates]
+
+    # Créer la matrice pour la carte de fréquentation
+    heatmap = np.zeros((len(y_labels), len(x_labels)))
+    for i, value in enumerate(values):
+        heatmap[y_indices[i], x_indices[i]] = value
+
+    # Afficher la carte de fréquentation avec la colormap
+    plt.imshow(heatmap, cmap="Blues", interpolation="nearest", vmin=0, vmax=10)
+    plt.xticks(range(len(x_labels)), x_labels)
+    plt.yticks(range(len(y_labels)), y_labels)
+    plt.xlabel("Learning Rate")
+    plt.ylabel("Batch Size")
+    plt.colorbar(label="Experiences", pad=0.02)
+
+    # Ajouter les valeurs à l'intérieur des cellules
+    for i in range(len(y_labels)):
+        for j in range(len(x_labels)):
+            value = heatmap[i, j]
+            if value < 6: color = "black"
+            elif value < 11: color = "white"
+            else: color = "red"
+            plt.text(j, i, str(int(value)), color=color, fontweight="bold", ha="center", va="center")
+
+    plt.title(f"Grille de {model}")
+    plt.show()
+
+if __name__ == "__main__":
+    drive = Gdrive.identification()
+    main(drive, model="Inception3_SGD_CNL2")
